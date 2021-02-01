105e976ac10d4687156a6b9abf375bf6d2930ded
toxelarin <59933982+toxelarin@users.noreply.github.com>
2021-02-01 21:28:57 +0300

    Update killerexsh.sh

--- killerexsh.sh
+++ killerexsh.sh
@@ -1,5 +1,5 @@
-cd ..
-sleep 1
-cd Impulse
-sleep 1
-python3 impulse.py --method SMS --time 90 --threads 100 --target +905368482015
+echo cd ..
+echo sleep 1
+echo cd Impulse
+echo sleep 1
+echo python3 impulse.py --method SMS --time 90 --threads 100 --target +905368482015
